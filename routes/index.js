2c9e54abd2818d2e8dcecaafa81743b79a1aa69a
Edwin Rivera <edwin.rivera@outlook.com>
2017-07-12 16:35:26 -0400

    covering the ideas behind using the pug templating engine with examples

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,17 @@
-// this file will handle any traffic to the site
-
-// grab express
 const express = require('express');
-// get the Router() method from express
 const router = express.Router();
 
-// callback function runs whenever someone hits the / url
-// request object full of the info coming in
-// response object full of methods for sending data back to user
-// never send data more than once using using because headers will already be sent
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-
-  // can send an object as JSON
-  // const person = {
-  //   name: 'Bob',
-  //   age: 33
-  // }
-  // res.json();
-
-  // can grab data from a URL using req.query.<name of parameter> and then send it back to the browser
-  // res.send(req.query.name);
+  // this will render the template file hello.pug
+  // data can be passed to the template as an object
+  // can additionally take in data from the URL using req.query
+  res.render('hello', {
+    // these are called local variables
+    name: 'Bob',
+    food: req.query.food
+  });
 });
 
-// req.params will access things in the URL
-// here we grab the name parameter from the URL then reverse it and send it back the browser
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
   res.send(reverse);
