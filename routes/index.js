078257165b47b19f67d79da8db8b8e7be9afe905
Edwin Rivera <edwin.rivera@outlook.com>
2017-07-13 15:57:20 -0400

    going over controllers and their place within the MVC patter

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,10 @@
 const express = require('express');
 const router = express.Router();
+// imports the controller that we want to make a route for
+const storeController = require('../controllers/storeController');
 
-router.get('/', (req, res) => {
-  // this will render the template file hello.pug
-  // data can be passed to the template as an object
-  // can additionally take in data from the URL using req.query
-  res.render('hello', {
-    // these are called local variables
-    name: 'Bob',
-    food: req.query.food,
-    // this will pass the title to the layout file
-    title: 'Some stuff here'
-  });
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+// bring the controller in here
+// always going to send the work to an external file
+router.get('/', storeController.homePage);
 
 module.exports = router;
