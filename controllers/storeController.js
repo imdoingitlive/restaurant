decebaf6caa7233a5fcff78938f116c97bfcaeba
Edwin Rivera <edwin.rivera@outlook.com>
2017-07-13 16:33:15 -0400

    reviewing concepts behind middleware and error handling

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,14 @@
+// when writing middleware, next must always be passed as an argument
+exports.myMiddleware = (req, res, next) => {
+  req.name = 'Bob';
+  // this will create a cookie using cookieParser
+  res.cookie('name', 'Steve', {maxAge: 300000});
+  // once the above is complete, next passes things off to the next function
+  next();
+}
+
+// any work that has to happen between the req and res should be handled with middleware
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-// imports the controller that we want to make a route for
 const storeController = require('../controllers/storeController');
 
-// bring the controller in here
-// always going to send the work to an external file
-router.get('/', storeController.homePage);
+// add the middleware
+// since the middleware has next() inside of it, it will pass to homePage
+// this is route specific middleware
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
+
+// express can have global middleware
+// will be application wide features and functions
+// example is app.js
